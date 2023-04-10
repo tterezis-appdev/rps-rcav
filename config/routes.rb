0a71353838ca121b171b7aa03dd232c8db7845eb
Teddy Terezis <tterezis@chicagobooth.edu>
2023-04-10 19:09:45 +0000

    Final Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+get("/", { :controller => "application", :action => "homepage" })
+
 get("/rock", { :controller => "application", :action => "play_rock" })
 
   get("/paper", { :controller => "application", :action => "play_paper" })
